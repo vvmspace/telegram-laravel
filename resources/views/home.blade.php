c9006593cf02ab072f0035c7105e503270a294bf
vvmspace <concertbil@gmail.com>
2017-10-16 01:14:52 +0500

    Created TelegramController + Routes + Home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+	    <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Telegram Bot Example Beta</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 21px;
+            }
+            
+        </style>
+	</head>
+	<body>
+		<div class="content">
+			<div class="title">Laravel Telegram Bot Example Beta</div>
+		</div>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'TelegramController@getHome');
+Route::get('get-updates',   'TelegramController@getUpdates');
